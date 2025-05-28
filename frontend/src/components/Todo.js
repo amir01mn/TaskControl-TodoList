414f829bfd7e2dc5ce74c46d337a1c0ec98ab74b
amirmansouri <amir01mansouri@gmail.com>
2025-05-28 15:56:50 -0400

    Finalize the Todo App, adding and deleting the list completed, check box update completed

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,13 +1,28 @@
 import { FaDeleteLeft } from "react-icons/fa6";
 
+import { useState } from "react";
 
-const Todo = ({todo_name}) =>{
+import { update_todo } from "../api/endpoints";
+
+const Todo = ({id, todo_name, completed, deleteTodo}) =>{
+
+    const [isChecked, setChecked] = useState(completed)
+
+    const handleDelete = async() => {
+       await deleteTodo(id);
+    }
+
+    const handleComplete = async() => {
+        update_todo(id, !isChecked)
+        setChecked(!isChecked)
+
+    }
     return(
         <div className='todo'>
             <div className='todo-container'>
-                <input type='checkbox' />
+                <input checked={isChecked} onClick={handleComplete} type='checkbox' />
                 <h3>{todo_name}</h3>
-                <FaDeleteLeft size = '20px'/>
+                <FaDeleteLeft onClick={handleDelete} size = '20px'/>
 
             </div>
 
--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,12 +1,12 @@
 import Todo from "./Todo";
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos, deleteTodo}) => {
     return(
         <div className= 'todo-list'>
             {
                 todos.map((todo) => {
                     return (
-                        <Todo />
+                        <Todo key={todo.id} id={todo.id} todo_name={todo.todo_name} completed={todo.completed} deleteTodo={deleteTodo} />
                     )
                 })
         
